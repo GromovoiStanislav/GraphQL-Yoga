99d571ffdb4462620fed663639d038c790046514
Gromovoi Stanislav <gromila12@rambler.ru>
2023-06-21 12:21:32 +0600

    project_3

--- project_3/dist/context.js
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-import { authenticateUser } from './auth.js';
-const prisma = new PrismaClient();
-export async function createContext(initialContext) {
-    return {
-        prisma,
-        currentUser: await authenticateUser(prisma, initialContext.request)
-    };
-}
